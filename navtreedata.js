3d7b7a127d87bbbfdac7af6f6d971a13a9a09386
Wolf3s <Wolf3s@users.noreply.github.com>
2025-01-18 10:08:43 +0000

    deploy: 610277e0488b789704b2a964d7094ba7bcd5ee99

---
+++ navtreedata.js
@@ -0,0 +1,88 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PS2SDK", "index.html", [
+    [ "PS2SDK - PS2DEV Open Source Project", "index.html", null ],
+    [ "PS2SDK - KERNEL LIBRARY", "md_ee_2kernel_2_r_e_a_d_m_e.html", null ],
+    [ "remote", "md_ee_2rpc_2remote_2remote.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"__start_8c_source.html",
+"atahw_8h.html#a414747b879d890c1df55ccbd4208cf1ca8b2c6c7e319818c77569ecb3e7b0f981",
+"audsrv_8c.html#addc7abfb9fc4d953e5e6bd5014948a3e",
+"devfs_8c.html#ae6a5c3fbbab232e10792ee97dfca8965",
+"dir_6b1fe306776c19ae02b5b1275d4c63ec.html",
+"dir_dd15e966dfe8a4239d7e9367008bc621.html",
+"draw__types_8h.html#struct____attribute_____8____unnamed50____",
+"ee__regs_8h.html#a7c40b051980b09e2c28ea039e92a3a53",
+"errno_8h.html#ad88bc6ea94ec1a5e91d3651677d85c00",
+"gif__registers_8h.html#a3bc68faccfcee73db0d0313f52b17b97",
+"group__iopmgr.html#ga863cdb9030dad2c7ab81cd36341d7d8d",
+"gs__gp_8h.html#a724cb3d528c08547513ef748620829f9",
+"intrman_8h.html#ae9e5efbc2fcad5254df314e8549da246a3ae73210f702264001b53e1d4a459077",
+"iop_2sound_2audsrv_2src_2adpcm_8c.html#a0d0a018c976774c24275d8acd30bc770",
+"iopdebug__defs_8h.html",
+"libcdvd-common_8h.html#ac09ca8831146c5703265482a78e7f2b1",
+"libgs_8h.html#a5664fba7030632d995226586bb7e6026",
+"libgs_8h.html#struct_g_s___f_o_g_c_o_l_o_r",
+"libmc_8h.html#a628b7db04235f228d40adc671413a8c8",
+"libvux_8h.html#struct_v_u___f_l_a_t___l_i_g_h_t",
+"osd__config_8c.html#struct_config_param_t10_k",
+"ps2cam_8h.html#a4afd582a279dd569c041b1d0057b3926",
+"rpc_2hdd_2src_2erl-support_8c_source.html",
+"sio2man_8h_source.html",
+"ssgcp_8c_source.html",
+"tcpip_8h.html#structsockaddr__in",
+"vif__codes_8h.html#a18248bde4a19b4d915dd7dc3fae4a338"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
